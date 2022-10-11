2940f38c300885e2de1852b06edad5afd4b608fe
tinchourteaga-ml <martin.urteaga@mercadolibre.com>
2022-10-11 11:11:31 -0300

    service and endpoints - store folder

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,64 +1,37 @@
 package main
 
 import (
-	"encoding/csv"
-	"fmt"
-	"os"
-	"strconv"
-
 	"github.com/gin-gonic/gin"
-	"github.com/tinchourteaga-ml/desafio-go-web-martin-urteaga/internal/domain"
+
+	"github.com/tinchourteaga-ml/desafio-go-web-martin-urteaga/cmd/server/handler"
+	"github.com/tinchourteaga-ml/desafio-go-web-martin-urteaga/internal/tickets"
+	"github.com/tinchourteaga-ml/desafio-go-web-martin-urteaga/pkg/store"
 )
 
 func main() {
 
 	// Cargo csv.
-	list, err := LoadTicketsFromFile("../../tickets.csv")
+	ticketList, err := store.LoadTicketsFromFile("../../tickets.csv")
+
 	if err != nil {
 		panic("Couldn't load tickets")
 	}
 
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) { c.String(200, "pong") })
-	// Rutas a desarollar:
+	repo := tickets.NewRepository(ticketList)
+	service := tickets.NewService(repo)
+	ticket := handler.NewService(service)
 
-	// GET - “/ticket/getByCountry/:dest”
-	// GET - “/ticket/getAverage/:dest”
-	if err := r.Run(); err != nil {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) { c.String(200, "pong") })
+
+	tickets := router.Group("/ticket")
+	{
+		tickets.GET("/getByCountry/:dest", ticket.GetTicketsByCountry())
+		tickets.GET("/getAverage/:dest", ticket.AverageDestination())
+	}
+
+	if err := router.Run(); err != nil {
 		panic(err)
 	}
 
-}
-
-func LoadTicketsFromFile(path string) ([]domain.Ticket, error) {
-
-	var ticketList []domain.Ticket
-
-	file, err := os.Open(path)
-	if err != nil {
-		return nil, fmt.Errorf("could not open file: %w", err)
-	}
-
-	csvR := csv.NewReader(file)
-	data, err := csvR.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("could not open file: %w", err)
-	}
-
-	for _, row := range data {
-		price, err := strconv.ParseFloat(row[5], 64)
-		if err != nil {
-			return []domain.Ticket{}, err
-		}
-		ticketList = append(ticketList, domain.Ticket{
-			Id:      row[0],
-			Name:    row[1],
-			Email:   row[2],
-			Country: row[3],
-			Time:    row[4],
-			Price:   price,
-		})
-	}
-
-	return ticketList, nil
 }
